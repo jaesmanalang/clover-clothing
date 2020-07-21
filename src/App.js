87543beca30a188cc17d807aafcffd6ad87176f9
James Manalang <jaesmanalang@gmail.com>
2020-07-21 20:57:12 +0800

    Update menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import HomePage from './components/pages/homepage/HomePage';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 const App = () => {
   return (
-    <div className="app">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop/hats" component={HatsPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
