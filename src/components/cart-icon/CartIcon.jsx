27b03cdb31c50d0e4a009bb4c8da0bec122eb8c8
James Manalang <jaesmanalang@gmail.com>
2020-07-22 23:55:53 +0800

    Add reselect library and implement memoization

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cartActions';
+import { selectCartItemsCount } from '../../redux/cart/cartSelectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './CartIcon.scss';
 
@@ -11,11 +12,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce(
-    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
-    0,
-  ),
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state),
 });
 
 const mapDispatchToProps = dispatch => ({
