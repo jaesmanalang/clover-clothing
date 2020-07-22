863693f2fd2ac4cfd2aa807f656d37a6db1590c2
James Manalang <jaesmanalang@gmail.com>
2020-07-23 01:28:19 +0800

    Add remove and add item to cart functionality

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -5,10 +5,11 @@ import { withRouter } from 'react-router-dom';
 import { selectCartItems } from '../../redux/cart/cartSelectors';
 import CustomButton from '../../components/custom-button/CustomButton';
 import CartItem from '../cart-item/CartItem';
+import { toggleCartHidden } from '../../redux/cart/cartActions';
 
 import './CartDropdown.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.length ? (
@@ -19,7 +20,12 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className="empty-message">Your cart is empty</span>
       )}
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>
+    <CustomButton
+      onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden());
+      }}
+    >
       GO TO CHECKOUT
     </CustomButton>
   </div>
